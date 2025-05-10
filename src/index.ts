efdf00ef81f29eacd5ccb797ac2b22b35c4d2d88
pravinpaudel <ppaudel@upei.ca>
2025-05-10 15:34:32 -0300

    Prisma setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-// import { prismaClient } from './lib/db';
+import { prismaClient } from './lib/db';
 
 async function startServer() {
     const app = express();
@@ -23,19 +23,19 @@ async function startServer() {
                 hello: () => 'Hello world!'
             },
             Mutation: {
-                // createUser: async (_, { firstName, lastName, email, password }:
-                //     { firstName: string; lastName: string; email: string; password: string }) => {
-                //     await prismaClient.user.create({
-                //         data: {
-                //             firstName,
-                //             lastName,
-                //             email,
-                //             password,
-                //             salt: "random_salt"
-                //         }
-                //     })
-                //     return true;
-                // }
+                createUser: async (_, { firstName, lastName, email, password }:
+                    { firstName: string; lastName: string; email: string; password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt: "random_salt"
+                        }
+                    })
+                    return true;
+                }
             }
         }
     });
